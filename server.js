941ff792669e9f1dec0b817c879bb6b59068fbbc
Thornhill420 <thornhill_mt@hotmail.co.uk>
2025-02-20 15:27:56 +0200

    Updated backend for Render deployment & updated ContactForm API URL

--- server.js
+++ server.js
@@ -63,4 +63,6 @@ app.post('/book-service', async (req, res) => {
     }
 });
 
-app.listen(5000, () => console.log("Backend running on http://localhost:5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, "0.0.0.0", () => console.log(`Backend running on port ${PORT}`));
+
